33fff06e2a06ca1645473ccf02b851d8dd409940
Brent <blthiewes@gmail.com>
2018-09-26 21:05:39 -0500

    add ts config changes and debug tasks

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -10,9 +10,10 @@ import { MessageService } from './message.service';
 export class HeroService {
 
   constructor(private messageService: MessageService) { }
-  
+
   getHeroes(): Observable<Hero[]> {
     this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    const herosOut = HEROES;
+    return of(herosOut);
   }
 }
