b945e80b95d49109c643d917648dbd6fa1901198
Brent <blthiewes@gmail.com>
2018-10-01 21:07:45 -0500

    finished routing

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -16,4 +16,10 @@ export class HeroService {
     const herosOut = HEROES;
     return of(herosOut);
   }
+
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
 }
