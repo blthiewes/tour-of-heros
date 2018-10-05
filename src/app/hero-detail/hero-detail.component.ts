de06c21388e1dc65c94e52287167abcbe526160e
Brent <blthiewes@gmail.com>
2018-10-04 19:29:13 -0500

    got to https://angular.io/tutorial/toh-pt6#add-heroservicedeletehero

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,17 +1,22 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
-  selector: 'app-hero-detail',
-  templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+	selector: 'app-hero-detail',
+	templateUrl: './hero-detail.component.html',
+	styleUrls: [ './hero-detail.component.css' ]
 })
 export class HeroDetailComponent implements OnInit {
-  @Input() hero: Hero;
+	@Input() hero: Hero;
 
-  constructor() { }
+	constructor(private heroService: HeroService) {}
 
-  ngOnInit() {
-  }
+	ngOnInit() {}
 
+	save(): void {
+		this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
+	}
+
+	goBack(): void {}
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,28 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import {Hero} from '../hero';
+import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+	selector: 'app-heroes',
+	templateUrl: './heroes.component.html',
+	styleUrls: [ './heroes.component.css' ]
 })
 export class HeroesComponent implements OnInit {
-  heroes: Hero[];
-  selectedHero: Hero;
+	heroes: Hero[];
+	selectedHero: Hero;
 
-  constructor(private heroService: HeroService) {
-   }
+	constructor(private heroService: HeroService) {}
 
-  ngOnInit() {
-    this.getHeroes();
-  }
+	ngOnInit() {
+		this.getHeroes();
+	}
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-  getHeroes(): void {
-    this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
-  }
+	delete(hero: Hero): void {
+		this.heroes = this.heroes.filter((h) => h !== hero);
+		this.heroService.deleteHero(hero).subscribe();
+	}
+
+	add(name: string): void {
+		name = name.trim();
+		if (!name) {
+			return;
+		}
+		this.heroService.addHero({ name } as Hero).subscribe((hero) => {
+			this.heroes.push(hero);
+		});
+	}
+
+	onSelect(hero: Hero): void {
+		this.selectedHero = hero;
+	}
+	getHeroes(): void {
+		this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+	}
 }
